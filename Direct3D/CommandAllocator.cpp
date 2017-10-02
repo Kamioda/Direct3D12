56ac85d24345aeb7f8d1af30418e4ec3a6cc2cfb
AinoMegumi <aimegu2014@gmail.com>
2017-10-03 01:21:01 +0900

    いらないファイルを削除

--- Direct3D/CommandAllocator.cpp
+++
@@ -1,7 +0,0 @@
-#include "Direct3D.hpp"
-#include "KgWinException.hpp"
-
-CommandAllocator::CommandAllocator(const Device Dev) {
-	const HRESULT hr = Dev->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&this->Dx12CommandAllocator));
-	if (FAILED(hr)) throw KgWinException("Failed Create Command Allocator");
-}
--- Direct3D/Fence.cpp
+++
@@ -1,8 +0,0 @@
-#include "Direct3D.hpp"
-#include "KgWinException.hpp"
-
-Fence::Fence(const Device Dev) {
-	const HRESULT hr = Dev->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&this->Dx12Fence));
-	if (FAILED(hr)) throw KgWinException(hr);
-	this->FenceValue = 1;
-}
